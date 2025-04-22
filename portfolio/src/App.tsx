1c034c2df3b0a79734335fdf2b4c4fd76a8c1e91
Shakiba Alipour <alipour.sh79@gmail.com>
2025-04-22 13:08:56 +0330

    Navbar is added

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <div className="w-full">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
