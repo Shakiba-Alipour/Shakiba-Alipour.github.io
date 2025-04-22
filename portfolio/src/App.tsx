06eebc3615c11d955d1a87792b3652ff8d3f1fe7
Shakiba Alipour <alipour.sh79@gmail.com>
2025-04-22 13:24:10 +0330

    footer is completed

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="w-full">
+    <div className="w-full h-full">
       <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ portfolio/src/components/Footer.tsx
@@ -0,0 +1,52 @@
+import { GithubFilled, LinkedinFilled, MailFilled } from "@ant-design/icons";
+import { Layout } from "antd";
+
+const { Footer: AntFooter } = Layout;
+
+const Footer = () => {
+  const logoClassName = "text-2xl mr-8 cursor-pointer hover:text-gray-300";
+
+  return (
+    <AntFooter
+      id="footer-section"
+      className=" absolute w-full bottom-0 bg-dark-gray text-white"
+    >
+      <div className="flex flex-row justify-between">
+        <div>
+          <a
+            id="linkedin"
+            href="https://www.linkedin.com/in/shakiba-alipour"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={logoClassName}
+          >
+            <LinkedinFilled />
+          </a>
+
+          <a
+            id="github"
+            href="https://github.com/Shakiba-Alipour"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={logoClassName}
+          >
+            <GithubFilled />
+          </a>
+
+          <a
+            id="email"
+            href="mailto:alipour.sh79@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={logoClassName}
+          >
+            <MailFilled />
+          </a>
+        </div>
+        <p>@ 2025 Shakiba Alipour. All rights reserved.</p>
+      </div>
+    </AntFooter>
+  );
+};
+
+export default Footer;
