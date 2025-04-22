be86e4d8b393d205ed4a93f8475295259924606f
Shakiba Alipour <alipour.sh79@gmail.com>
2025-04-22 13:36:01 +0330

    home is completed

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,14 @@
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="w-full h-full">
       <Navbar />
+      <main>
+        <Home />
+      </main>
       <Footer />
     </div>
   );
---
+++ portfolio/src/components/Home.tsx
@@ -0,0 +1,21 @@
+const Home = () => {
+  return (
+    <div
+      id="home-section"
+      className="w-full items-center content-center flex justify-center py-8"
+    >
+      <div className="text-center flex flex-col justify-center my-8">
+        <h1 className="text-2xl font-bold">Hi there!</h1>
+        <h1 className="text-4xl font-bold my-10">I'm Shakiba Alipour</h1>
+        <p className="hyphens-auto text-xl w-3/4 2xl:1/2 xl:1/2 lg:w-1/2 self-center">
+          A computer engineer, passionate about software engineering and web
+          development. Alongside my technical pursuits, enthusiastic about team
+          management and strive to embrace a multidisciplinary approach in my
+          work.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
