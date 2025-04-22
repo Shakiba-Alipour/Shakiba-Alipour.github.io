709b825660a6c2be7ee74919ac1796fe54c41743
Shakiba Alipour <alipour.sh79@gmail.com>
2025-04-22 13:08:29 +0330

    tailwind configuration

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -1,8 +1,12 @@
+import customTheme from "./theme";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: customTheme.colors,
+    },
   },
   plugins: [],
 };
---
+++ portfolio/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+  colors: {
+    "dark-gray": "#333",
+  },
+};
+
+export default theme;
