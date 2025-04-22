02272195de23fe2c4e9bb59a225a7ffd0aaecc49
Shakiba Alipour <alipour.sh79@gmail.com>
2025-04-22 10:29:35 +0330

    vite configuration

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Shakiba-Alipour.github.io/",
-  server: {
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  plugins: [react(), tailwindcss()],
+  base: "/",
 });
