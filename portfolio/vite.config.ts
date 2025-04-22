1cef0253e88145b3565ba0c2564a2dca2a1d0e94
Shakiba Alipour <alipour.sh79@gmail.com>
2025-04-23 00:19:32 +0330

    build

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Shakiba-Alipour.github.io/",
+  base: "/Shakiba-Alipour.github.io",
   resolve: {
     alias: {
       "@": "/src",
